aea969f84f9dd5d7c9dd7963bc5570e1a4fbfd30
ClaudioJayme <claudiojayme@gmail.com>
2024-06-10 07:49:00 -0300

    first commit

---
+++ src/componentes/listaClientes.tsx
@@ -0,0 +1,77 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+type props = {
+    tema: string
+};
+
+type state = {
+    selectedItem: string | null,
+    showModal: boolean
+};
+
+export default class ListaCliente extends Component<props, state> {
+    constructor(props: props | Readonly<props>) {
+        super(props);
+        this.state = {
+            selectedItem: null,
+            showModal: false
+        };
+        this.handleItemClick = this.handleItemClick.bind(this);
+        this.handleCloseModal = this.handleCloseModal.bind(this);
+    }
+
+    handleItemClick(item: string, e: React.MouseEvent) {
+        e.preventDefault();
+        this.setState({
+            selectedItem: item,
+            showModal: true
+        });
+    }
+
+    handleCloseModal() {
+        this.setState({ showModal: false });
+    }
+
+    render() {
+        const { tema } = this.props;
+        const { selectedItem, showModal } = this.state;
+        return (
+            <main>
+                <div className="list-group">
+                    {["Cliente 1", "Cliente 2", "Cliente 3", "Cliente 4", "Cliente 5", "Cliente 6"].map(cliente => (
+                        <a
+                            href="#"
+                            key={cliente}
+                            className={`list-group-item list-group-item-action list-group-item-primary ${selectedItem === cliente ? tema : ""}`}
+                            onClick={(e) => this.handleItemClick(cliente, e)}
+                        >
+                            {cliente}
+                        </a>
+                    ))}
+                </div>
+                {showModal && (
+                    <div className="modal show" tabIndex={-1} style={{ display: "block", backgroundColor: "rgba(0, 0, 0, 0.5)" }}>
+                        <div className="modal-dialog">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h5 className="modal-title">Detalhes do Cliente</h5>
+                                    <button type="button" className="btn-close" onClick={this.handleCloseModal}></button>
+                                </div>
+                                <div className="modal-body">
+                                    <p>nome: {selectedItem}</p>
+                                    <p>nome social: {selectedItem}</p>
+                                    <p>email: {selectedItem}@email.com</p>
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="button" className="btn" onClick={this.handleCloseModal}>Fechar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </main>
+        );
+    }
+}
---
+++ src/componentes/roteador.tsx
@@ -0,0 +1,52 @@
+import { Component } from "react";
+import BarraNavegacao from "./barraNavegacao";
+import ListaCliente from "./listaClientes";
+import FormularioCadastroCliente from "./formularioCadastroCliente";
+
+type state = {
+    tela: string
+}
+
+export default class Roteador extends Component<{}, state>{
+    constructor(props: {} | Readonly<{}>) {
+        super(props)
+        this.state = {
+            tela: 'Clientes'
+        }
+        this.selecionarView = this.selecionarView.bind(this)
+    }
+
+    selecionarView(novaTela: string, evento: Event) {
+        evento.preventDefault()
+        console.log(novaTela);
+        this.setState({
+            tela: novaTela
+        })
+    }
+
+    render() {
+        let barraNavegacao = <BarraNavegacao seletorView={this.selecionarView} tema="#99ccff" botoes={['Clientes', 'Cadastros', 'Produtos']} />
+        if (this.state.tela === 'Clientes') {
+            return (
+                <>
+                    {barraNavegacao}
+                    <ListaCliente tema="#e3f2fd" />
+                </>
+            )
+        }if(this.state.tela === 'Protudos'){
+            return(
+                <>
+                
+                </>
+            )
+
+        }else {
+            return (
+                <>
+                    {barraNavegacao}
+                    <FormularioCadastroCliente tema="#e3f2fd" />
+                </>
+            )
+        }
+    }
+}
